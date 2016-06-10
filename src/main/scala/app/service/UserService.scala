35fab7e2115b91705d4497367b1ef1bebfe530e9
sl <sl@anahoret.com>
2016-06-10 12:56:51 +0200

    Add UserHolder.

--- src/main/scala/app/service/UserService.scala
+++ src/main/scala/app/service/UserService.scala
@@ -9,6 +9,16 @@ private[service] object Users {
   )
 }
 
+class UserHolder(val currentUser: Option[User]) {
+  private[service] def findUser(username: String, password: String): Option[User] =
+    Users.users.find(u => u.username == username && u.password == password)
+
+  def login(username: String, password: String): UserHolder =
+    new UserHolder(findUser(username, password))
+
+  def logout: UserHolder = new UserHolder(None)
+}
+
 private[service] object CurrentUser {
   private var user: Option[User] = None
 
